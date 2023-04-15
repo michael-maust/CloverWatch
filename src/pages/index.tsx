4c2b8c027f4be83ceb00563d243f6b3543fc2327
Michael Maust <info@michaelmaust.com>
2023-04-15 13:37:06 -0500

    Finishes landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,42 @@ import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import Account from '../components/Account'
+import Image from 'next/image'
+import logo from 'public/logo.svg'
+import headerImage from 'public/coverImage.jpg'
+
+const FEATURES = ['Crop Rotation Scheduling', 'Soil Analysis', 'Field Mapping', 'Weather Forecasting', 'Farm Management', 'Treatment Scheduling']
+
 
 const Home = () => {
   const session = useSession()
   const supabase = useSupabaseClient()
 
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+    <div className="h-screen w-screen flex justify-center items-center">
       {!session ? (
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={['google']} theme="dark" />
+        <div className="grid grid-cols-2  w-screen h-screen p-6 justify-center items-center">
+
+          <div className="px-12 rounded-2xl shadow-sm bg-tan flex flex-col h-fit py-16 justify-center gap-6 border-2 border-gray">
+            <Image src={logo} alt='header image' width={300} height={300} />
+            <h2 className="text-5xl text-charcoal whitespace-pre font-bold">{`The Future of\nField Management`}</h2>
+            <p className="max-w-[500px] text-2xl text-charcoal">CloverWatch is the modern solution for managing your fields and simplifying farm operations. </p>
+
+            <div className="flex flex-wrap gap-3">
+              {FEATURES.map((feature) => (
+                <div key={feature} className="w-fit shadow-md hover:scale-[1.01] cursor-default text-tan text-xl px-2 py-1 rounded-lg items-center  bg-charcoal">
+                  {feature}
+                </div>
+              ))}
+            </div>
+          </div>
+
+
+          <div className="px-12 my-auto ">
+            <p className="mx-auto text-green text-center text-3xl font-bold mb-3">Modernize your farm. Join Now.</p>
+            <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={['google']} />
+          </div>
+        </div>
       ) : (
         <Account session={session} />
       )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+
+      fontFamily: {
+        sans: ["Nunito", "Helvetica Neue"],
+      },
+
+      colors: {
+        tan: "#F7F6F0",
+        charcoal: "#495057",
+        blue: "#457B9D",
+        green: "#588157",
+        lightGreen: "#CFDFCE",
+        orange: "#E26D5A",
+
+        gray: {
+          100: "#eeeeee",
+          200: "#e0e0e0",
+          300: "#bbbbbb",
+          400: "#666666",
+          500: "#444444",
+          600: "#2a2a2a",
+          700: "#1f1f1f",
+          800: "#181818",
+          900: "#0f0f0f",
+        },
       },
     },
   },
   plugins: [],
-}
+};
